d5cc0322812e7bb5ce22ae3ee05b7f4043185bd5
renxun <renxun@doupai.cc>
2018-10-26 18:46:35 +0800

    可选,类型转化拓展,协议(未完)

--- PlaygroundFile/Swift4.2.playground/Pages/Extension.xcplaygroundpage/Contents.swift
+++ PlaygroundFile/Swift4.2.playground/Pages/Extension.xcplaygroundpage/Contents.swift
@@ -5,3 +5,64 @@ import Foundation
 //扩展就是为一个已有的类,结构体,枚举类型或者协议类型添加新功能.扩展和 Objective-C 中的分类类似
 
 //在Swfit中,你可以对协议进行拓展,.提供协议要求的实现,或者添加额外的功能,从而让协议也拥有这些功能
+
+/*
+ Swift中的拓展可以
+ * 添加计算型属性和计算类型属性
+ * 定义对象方法,类方法
+ * 定义下标
+ * 定义和使用新内嵌类型
+ * 使现有的类型遵循某协议
+ 
+ */
+
+protocol Protocol1 {
+    
+}
+
+protocol Protocol2 {
+    
+}
+
+//拓展的语法
+
+class A {
+    
+}
+
+extension A {
+    
+}
+
+//遵循多个协议
+extension A: Protocol1,Protocol2 {
+    
+    
+}
+
+//计算属性
+extension Int {
+    var myDouble:Double {return Double(self)}
+}
+
+10.myDouble
+
+//拓展构造器
+struct Size {
+    var width = 0.0,height = 0.0
+}
+
+extension Size {
+    init(MyWidth:Double,MyHeight:Double) {
+    }
+    
+    //异变实例方法 (结构体还有枚举)
+    mutating func modifyValue(newWidth:Double) -> Void {
+        width = newWidth
+    }
+}
+
+
+
+
+
---
+++ PlaygroundFile/Swift4.2.playground/Pages/Protocol.xcplaygroundpage/Contents.swift
@@ -0,0 +1,189 @@
+
+//: [Previous](@previous)
+
+import Foundation
+
+//协议语法:
+protocol SomeProtocol {
+    // 这里是协议的定义部分
+}
+
+//多协议 用,区分
+
+class A {
+    
+}
+
+//有父类的情况下 父类在协议前面
+class B: A,SomeProtocol {
+    
+}
+
+
+
+//- 属性的要求
+
+//协议可以要求提供 特定的 实例属性,或者类属性, 不指定是计算型属性还是存储型属性,协议还指定当前属性是否是可读,可写的
+
+protocol protocol1 {
+    var mustBeSettable: Int { get set }
+    var doesNotNeedToBeSettable: Int { get }
+    static var someProperty: Int { get set }
+}
+
+protocol FullNameProtocol {
+    
+    var fullName: String {get}
+    
+}
+
+struct Man: FullNameProtocol {
+    var fullName: String
+}
+
+let john = Man(fullName: "xiaohua")
+
+// 定义方法
+
+protocol SomeProtocol2 {
+    static func someTypeMethod()
+    func logName() -> Double
+    init(someParam: Int) //协议的构造器
+
+}
+
+
+
+//有条件的遵循协议
+
+protocol ProtocolA {
+      func logPa() -> Void;
+}
+
+protocol ProtocolB {
+
+    func logPb() -> Void;
+    
+}
+
+class C: SomeProtocol2  {
+    
+    static func someTypeMethod() {
+        
+    }
+    
+    func logName() -> Double {
+        return 0.0
+    }
+    
+    
+    required init(someParam: Int) {
+    }
+}
+
+
+extension C: ProtocolA {
+    func logPa() {
+        print("遵守A协议")
+    }
+    
+}
+
+let cObj = C(someParam: 1)
+cObj.logPa()
+
+
+//声明采纳协议
+//如果一个类已经遵循了所有的需求,但是没有声明这个协议,这时候可以通过空的拓展表示已经遵循过了协议
+
+
+protocol ProtocolC {
+    func logPC() -> Void;
+}
+
+class D {
+    func logPC() {
+        print("遵守了ProtocolC协议")
+    }
+}
+
+extension D: ProtocolC {
+
+}
+
+//类专用的协议
+//通过添加 AnyObject 关键字到协议的继承列表，你就可以限制协议只能被类类型采纳（并且不是结构体或者枚举）。
+protocol SomeClassOnlyProtocol: AnyObject {
+
+}
+
+//标明参数遵守协议
+// protocolA & protocolB
+protocol Named {
+    var name: String { get }
+}
+protocol Aged {
+    var age: Int { get }
+}
+struct Person: Named, Aged {
+    var name: String
+    var age: Int
+}
+
+func wishHappyBirthday(to celebrator: Named & Aged ) {
+    print("Happy birthday, \(celebrator.name), you're \(celebrator.age)!")
+}
+
+//同时限定协议
+func wishHappyBirthday2<T: Named & Aged>(to celebrator2:T) {
+    print("Happy birthday2, \(celebrator2.name), you're \(celebrator2.age)!")
+
+}
+
+let birthdayPerson = Person(name: "Malcolm", age: 21)
+
+wishHappyBirthday(to: birthdayPerson)
+wishHappyBirthday2(to: birthdayPerson)
+
+// 可以使用 is 和as 类检查协议遵循能转换为特定的协议。 转换类型,跟转换协议是一样的
+protocol HasMoney {
+    var rich: Bool {get}
+}
+
+class Man1: HasMoney {
+    var rich: Bool = false
+    
+}
+
+class Man2: HasMoney {
+    var rich: Bool = true
+}
+
+class Man3 {
+    var rich: Bool = true
+}
+
+let manList: [AnyObject] = [Man1(),Man2(),Man3()]
+
+for obj in manList {
+    if let objecMoney = obj as? HasMoney { //检查是否遵循HasMoney协议
+        print("这个人是rich? :\(objecMoney.rich)")
+    }else if let manObj = obj as? Man3 { //检查是否是 Man3协议
+        print("Man3:\(manObj.rich)")
+    }else {
+        print("没有遵循协议 - 也不是Man3类型 \(obj)")
+    }
+}
+
+//可选协议
+//optional修饰, 协议和可选要求又必须使用 @objc标志标记 @obj 适用于 OC的类使用
+
+
+@objc protocol dataSource {
+  @objc optional func numOfCount() -> Int;
+
+}
+
+
+
+
