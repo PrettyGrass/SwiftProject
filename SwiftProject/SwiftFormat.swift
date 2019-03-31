78d6ffc47b7f53fd7426be5bd8f5724aa917ed58
ylin.yang <yangyuanlin@doupai.cc>
2019-03-31 15:36:28 +0800

    测试格式

---
+++ SwiftProject/SwiftFormat.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftFormat.swift
+//  SwiftProject
+//
+//  Created by ylin.yang on 2019/3/31.
+//  Copyright © 2019 renxun. All rights reserved.
+//
+
+import Foundation
+
+class SwiftFormat: Any {
+    
+    
+    
+    
+    
+    
+    
+    /// 多余的换行
+    func emptyBraces() {
+        
+    }
+}
